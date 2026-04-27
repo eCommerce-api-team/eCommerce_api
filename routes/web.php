a0caee2d97206e04f412869b46e3e1fc03220e93
nada asfour <nadaibraheem657@gmail.com>
2026-04-27 09:44:56 +0300

    make mailtrap account

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\WelcomeEmail;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/test-mail', function () {
+
+    $user = (object)[
+        'name' => 'Nada',
+        'email' => 'test@test.com'
+    ];
+
+    Mail::to($user->email)->send(new WelcomeEmail($user));
+
+    return 'Mail sent!';
 });
